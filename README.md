7b838a690c1d7b7948480a54a9899eeed29f0e27
soubhagyasethy <ssethy017@gmail.com>
2024-02-25 23:56:14 +0530

    task-5: Creating Elements & DOM Relations

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## task-4
 
-- querySelector & querySelectorAll Methods
+- Creating Elements & DOM Relations
+
+1. Inside the first Div, after the main heading add a sub-heading (h3 tag) "Buy high quality organic fruits online".
+
+2. Make the sub-heading text italic.
+
+3. Inside the second Div, before the unordered list add a paragraph tag showing "Total fruits: 4".
+
+4. On this paragraph tag set an id of "fruits-total".
--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-// Write the code as shown in the video below:
-let mainHeading = document.querySelector("#main-heading");
-mainHeading.style.textAlign = "right";
-mainHeading.style.marginRight = "40px";
+// Write your code below:
+// Add sub-heading inside the first div
+const mainHeading = document.getElementById("main-heading");
+const subHeading = document.createElement("h3");
+subHeading.textContent = "Buy high quality organic fruits online";
+subHeading.style.fontStyle = "italic";
+mainHeading.after(subHeading);
 
-let fruits = document.querySelector(".fruits");
-fruits.style.backgroundColor = "grey";
-fruits.style.padding = "30px";
-fruits.style.margin = "30px";
-fruits.style.width = "50%";
-fruits.style.borderRadius = "5px";
-fruits.style.listStyleType = "none";
-
-let basketHeading = document.querySelector("#basket-heading");
-basketHeading.style.color = "brown";
-basketHeading.style.marginLeft = "30px";
-
-let fruit = document.querySelectorAll(".fruit");
-for (let i = 0; i < fruit.length; i++) {
-  fruit[i].style.backgroundColor = "white";
-  fruit[i].style.padding = "30px";
-  fruit[i].style.margin = "30px";
-  fruit[i].style.borderRadius = "5px";
-}
-
-let oddFruit = document.querySelectorAll(".fruit:nth-child(odd)");
-for (let i = 0; i < oddFruit.length; i++) {
-  oddFruit[i].style.backgroundColor = "lightgrey";
-}
-
-let evenFruit = document.querySelectorAll(".fruit:nth-child(even)");
-for (let i = 0; i < evenFruit.length; i++) {
-  evenFruit[i].style.color = "white";
-  evenFruit[i].style.backgroundColor = "brown";
-}
+// Add paragraph tag showing total fruits inside the second div
+const basketHeading = document.getElementById("basket-heading");
+const totalFruits = document.createElement("p");
+totalFruits.textContent = "Total fruits: 4";
+totalFruits.id = "fruits-total";
+basketHeading.before(totalFruits);
